1db682b278d082fa043b933766addc66e4f6af80
Kacper <kacperzawadzki@gmail.com>
2019-05-01 19:00:55 +0200

    Is working

--- KZ_CA_2D/Form1.Designer.cs
+++ KZ_CA_2D/Form1.Designer.cs
@@ -29,16 +29,21 @@
         private void InitializeComponent()
         {
             this.buttonStart = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.numericUpDownMeshSizeX = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.trackBarCurrentTime = new System.Windows.Forms.TrackBar();
             this.numericUpDownMeshSizeY = new System.Windows.Forms.NumericUpDown();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.numericUpDownCurrentTime = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonFaster = new System.Windows.Forms.Button();
+            this.buttonSlower = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMeshSizeX)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBarCurrentTime)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMeshSizeY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCurrentTime)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonStart
@@ -51,17 +56,6 @@
             this.buttonStart.UseVisualStyleBackColor = true;
             this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.pictureBox1.Location = new System.Drawing.Point(9, 82);
-            this.pictureBox1.MinimumSize = new System.Drawing.Size(1000, 500);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1000, 500);
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            // 
             // numericUpDownMeshSizeX
             // 
             this.numericUpDownMeshSizeX.Location = new System.Drawing.Point(96, 30);
@@ -69,10 +63,11 @@
             this.numericUpDownMeshSizeX.Size = new System.Drawing.Size(120, 22);
             this.numericUpDownMeshSizeX.TabIndex = 7;
             this.numericUpDownMeshSizeX.Value = new decimal(new int[] {
-            100,
+            20,
             0,
             0,
             0});
+            this.numericUpDownMeshSizeX.ValueChanged += new System.EventHandler(this.numericUpDownMeshSizeX_ValueChanged);
             // 
             // label3
             // 
@@ -108,10 +103,64 @@
             this.numericUpDownMeshSizeY.Size = new System.Drawing.Size(120, 22);
             this.numericUpDownMeshSizeY.TabIndex = 12;
             this.numericUpDownMeshSizeY.Value = new decimal(new int[] {
-            100,
+            20,
             0,
             0,
             0});
+            this.numericUpDownMeshSizeY.ValueChanged += new System.EventHandler(this.numericUpDownMeshSizeY_ValueChanged);
+            // 
+            // numericUpDownCurrentTime
+            // 
+            this.numericUpDownCurrentTime.Location = new System.Drawing.Point(516, 10);
+            this.numericUpDownCurrentTime.Name = "numericUpDownCurrentTime";
+            this.numericUpDownCurrentTime.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownCurrentTime.TabIndex = 13;
+            this.numericUpDownCurrentTime.ValueChanged += new System.EventHandler(this.numericUpDownCurrentTime_ValueChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 83);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1000, 500);
+            this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Losowy",
+            "Pusty",
+            "Niezmienny",
+            "Glider",
+            "Oscylator"});
+            this.comboBox1.Location = new System.Drawing.Point(643, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // buttonFaster
+            // 
+            this.buttonFaster.Location = new System.Drawing.Point(771, 9);
+            this.buttonFaster.Name = "buttonFaster";
+            this.buttonFaster.Size = new System.Drawing.Size(75, 23);
+            this.buttonFaster.TabIndex = 16;
+            this.buttonFaster.Text = "Faster";
+            this.buttonFaster.UseVisualStyleBackColor = true;
+            this.buttonFaster.Click += new System.EventHandler(this.buttonFaster_Click);
+            // 
+            // buttonSlower
+            // 
+            this.buttonSlower.Location = new System.Drawing.Point(771, 39);
+            this.buttonSlower.Name = "buttonSlower";
+            this.buttonSlower.Size = new System.Drawing.Size(75, 23);
+            this.buttonSlower.TabIndex = 17;
+            this.buttonSlower.Text = "Slower";
+            this.buttonSlower.UseVisualStyleBackColor = true;
+            this.buttonSlower.Click += new System.EventHandler(this.buttonSlower_Click);
             // 
             // Form1
             // 
@@ -119,20 +168,25 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(1182, 653);
+            this.Controls.Add(this.buttonSlower);
+            this.Controls.Add(this.buttonFaster);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.numericUpDownCurrentTime);
             this.Controls.Add(this.numericUpDownMeshSizeY);
             this.Controls.Add(this.trackBarCurrentTime);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.numericUpDownMeshSizeX);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.buttonStart);
             this.MinimumSize = new System.Drawing.Size(1200, 700);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMeshSizeX)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBarCurrentTime)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMeshSizeY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCurrentTime)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,12 +195,16 @@
         #endregion
 
         private System.Windows.Forms.Button buttonStart;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.NumericUpDown numericUpDownMeshSizeX;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TrackBar trackBarCurrentTime;
         private System.Windows.Forms.NumericUpDown numericUpDownMeshSizeY;
+        private System.Windows.Forms.NumericUpDown numericUpDownCurrentTime;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonFaster;
+        private System.Windows.Forms.Button buttonSlower;
     }
 }
 
--- KZ_CA_2D/Form1.cs
+++ KZ_CA_2D/Form1.cs
@@ -13,79 +13,287 @@ namespace KZ_CA_1D
 {
     public partial class Form1 : Form
     {
-        Bitmap image1;
-        int meshSizeX;
-        int meshSizeY;
+        uint meshSizeX;
+        uint meshSizeY;
         int maxTime;
         int currentTime;
-        int[,] universe;
         List<int[,]> timespace;
         int delayTime;
         private volatile bool stopThread = false;
         private Thread workThread;
+        private Thread drawThread;
         bool flag = false;
+        bool reset = false;
+        int[,] currentUniverse;
 
         void initEvolution2D()
         {
-            meshSizeX = (int)numericUpDownMeshSizeX.Value;
-            meshSizeY = (int)numericUpDownMeshSizeY.Value;
+            meshSizeX = (uint)numericUpDownMeshSizeX.Value;
+            meshSizeY = (uint)numericUpDownMeshSizeY.Value;
             currentTime = 0;
             maxTime = 0;
-            delayTime = 0;
+            delayTime = 1000;
 
-            universe = new int[meshSizeX + 2, meshSizeY + 2];//+2 na warunki brzegowe
-            
-
-
+            //universe = new int[meshSizeX + 2, meshSizeY + 2];//+2 na warunki brzegowe //
+            timespace = null;
+            timespace = new List<int[,]>();
+            currentUniverse = null; ;
+            currentUniverse = new int[meshSizeX + 2, meshSizeY + 2];
+            seed();
+            //Początkowa wartośc universum
+            timespace.Add((int[,])currentUniverse.Clone());
+            System.Console.Write(2);
         }
-
-        void checkRule(int time, int cell)//otrzymuje krok czasowy i numer komórki
+        void resetEvolution()
         {
 
         }
 
-
-        void startEvolution2D()//będzie tu jakieś warunki brzegowe etc
+        void seed()
         {
+            Random random = new Random();
+            int switch_on = comboBox1.SelectedIndex;
+            int x = random.Next(2, (int)meshSizeX - 5);
+            int y = random.Next(2, (int)meshSizeY - 5);
+            switch (switch_on)
+            {
+                case 0://Random
+                    for (int i = 1; i < meshSizeX+1; i++)
+                    {
+                        for (int j = 1; j < meshSizeY+2; j++)
+                        {
+                            currentUniverse[i,j] = random.Next(0, 2);
+                        }
+                    }
+                    break;
+                case 1://Empty
+                    break;
+                case 2://Niezmienny                
+                    int l = 2;
+                    currentUniverse[x, y] = 1;
+                    currentUniverse[x, y + l + 1] = 1;
+                    for (int i = 1; i <= l; i++)
+                    {
+                        currentUniverse[x-  1, y + i] = 1;
+                        currentUniverse[x + 1, y + i] = 1;
+                    }
+                    break;
+                case 3://Glider
+                    currentUniverse[x - 1, y] = 1;
+                    currentUniverse[x - 1, y + 1] = 1;
+                    currentUniverse[x, y] = 1;//srodek
+                    currentUniverse[x, y - 1] = 1;
+                    currentUniverse[x + 1, y + 1] = 1;
+                    break;
+                case 4://Oscylator
+                    if (x%2 == 0)
+                    {
+                        currentUniverse[x - 1, y] = 1;
+                        currentUniverse[x, y] = 1;
+                        currentUniverse[x + 1, y] = 1;
+                    }
+                    else
+                    {
+                        currentUniverse[x, y - 1] = 1;
+                        currentUniverse[x, y] = 1;
+                        currentUniverse[x, y + 1] = 1;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
 
+
+        void setDataGridView()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            meshSizeX = (uint)numericUpDownMeshSizeX.Value;
+            meshSizeY = (uint)numericUpDownMeshSizeY.Value;
+            dataGridView1.ShowCellToolTips = false;
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+
+            dataGridView1.CurrentCell = null;
+            dataGridView1.ReadOnly = true;//!!!!!!!!!
+
+            dataGridView1.SuspendLayout();
+            for (int i = 0; i < meshSizeY; i++)
+            {
+                dataGridView1.Columns.Add("", "");
+                dataGridView1.Columns[i].Width = 10;
+            }
+
+            dataGridView1.Rows.Add((Int32)meshSizeX-1);
+
+            for (int i = 0; i < meshSizeX; i++)
+            {
+                dataGridView1.Rows[i].Height = 10;
+            }
+            dataGridView1.ResumeLayout();
 
         }
 
         public Form1() // zmienić te wartości na jakieś żeby się mieściły w pictureBox
         {
             InitializeComponent();
-            pictureBox1.Size = new Size(1000,500);
 
-            numericUpDownMeshSizeX.Minimum = 50;
-            numericUpDownMeshSizeX.Maximum = 1000;
-            numericUpDownMeshSizeY.Minimum = 50;
-            numericUpDownMeshSizeY.Maximum = 1000;
+
+            numericUpDownMeshSizeX.Minimum = 5;
+            numericUpDownMeshSizeX.Maximum = 60;
+            numericUpDownMeshSizeY.Minimum = 5;
+            numericUpDownMeshSizeY.Maximum = 100;
+            numericUpDownCurrentTime.Minimum = 0;
+            numericUpDownCurrentTime.Maximum = int.MaxValue;
+            numericUpDownCurrentTime.Enabled = false;
+            trackBarCurrentTime.Minimum = 0;
+            trackBarCurrentTime.Maximum = int.MaxValue;
+            trackBarCurrentTime.Enabled = false;
+
+
+            dataGridView1.Height = 600;
+            dataGridView1.Width = 1000;
+
+            meshSizeX = (uint)numericUpDownMeshSizeX.Value;
+            meshSizeY = (uint)numericUpDownMeshSizeY.Value;
+
+            setDataGridView();
+            comboBox1.SelectedIndex = 0;
+            initEvolution2D();
+        }
+
+
+        private int[,] setBoundaryCondition(int[,] universe)
+        {
+            int[,] tmpUniverse = new int[meshSizeX + 2, meshSizeY + 2];
+            tmpUniverse = (int[,])universe.Clone();
+            if (true)//meshSizeX == meshSizeY)//Dunno czy ten warunek jest potrzebny
+            {
+                //corners
+                tmpUniverse[0, 0] = tmpUniverse[meshSizeX, meshSizeY];//left up
+                tmpUniverse[0, meshSizeY + 1] = tmpUniverse[meshSizeX, 1];//right up
+                tmpUniverse[meshSizeX + 1, 0] = tmpUniverse[1, meshSizeY];//left down
+                tmpUniverse[meshSizeX + 1, meshSizeY + 1] = tmpUniverse[1, 1];//right down
+
+                //walls
+                //for (int i = 1; i < meshSizeX + 1; i++)
+                //{
+                //    tmpUniverse[0, i] = tmpUniverse[meshSizeX, i];//up wall
+                //    tmpUniverse[meshSizeX + 1, i] = tmpUniverse[1, i];//down wall
+                //}
+                //for (int i = 1; i < meshSizeY+ 1 ; i++)
+                //{
+                //    tmpUniverse[i, 0] = tmpUniverse[i, meshSizeY]; //left wall
+                //    tmpUniverse[i, meshSizeY + 1] = tmpUniverse[i, 0]; //right wall
+                //}
+                for (int i = 1; i < meshSizeX + 1; i++)
+                {
+                    tmpUniverse[i, 0] = tmpUniverse[i, meshSizeY]; //left wall
+                    tmpUniverse[i, meshSizeY + 1] = tmpUniverse[i, 0]; //right wall
+                }
+                for (int i = 1; i < meshSizeY + 1; i++)
+                {
+                    tmpUniverse[0, i] = tmpUniverse[meshSizeX, i];//up wall
+                    tmpUniverse[meshSizeX + 1, i] = tmpUniverse[1, i];//down wall
+                }
+
+
+            }
+            else
+            {
+
+            }
+
+            return tmpUniverse;
+        }
+
+        private int checkSurrounding(int[,] universe, int cellState,int x,int y)
+        {
+            int count = 0;
+            for (int i = x - 1; i < x + 2; i++)
+            {
+                for (int j = y - 1; j < y + 2; j++)
+                {
+                    if (i == x && j == y)
+                    {
+                        continue;
+                    }
+
+                    if (universe[i,j] == 1)
+                    {
+                        count += 1;
+                    }
+                }
+            }
+
+            if (cellState == 0)
+            {
+                if (count == 3) return 1; //Sprawdzić czy ==3 czy >= 3
+                else return 0;
+            }
+            else
+            {
+                if (count == 2 || count == 3) return 1;
+                else if (count < 2) return 0;
+                else if (count > 3) return 0;
+                    
+            }
+            return 1;
+        }
+        
+
+        private void grow(int time)//
+        {
+            
+            timespace[time-1] = setBoundaryCondition(timespace[time - 1]);
+            for (int i = 1; i < meshSizeX + 1; i++)
+            {
+                for (int j = 1; j < meshSizeY + 1; j++)
+                {
+                    timespace[time][i,j] = checkSurrounding(timespace[time - 1], timespace[time - 1][i, j], i, j); //sprawdza porzedni
+                }
+            }
+
         }
 
         private void evolution()
         {
-            do
+            while (true)
             {
+                maxTime++;
+                currentTime++;
+                //int[,] tmpUniverse = (int[,])timespace[currentTime].Clone();//Kopiuje obecny Może być tabula rasa?
+                timespace.Add(new int[meshSizeX + 2, meshSizeY + 2]);
+                grow(currentTime);
+                //----
+                currentUniverse = timespace[currentTime];
+                drawUniverse();
+                //dataGridView1.BeginInvoke((MethodInvoker)delegate
+                //{
+                //    drawUniverse();
+                //});
+                buttonStart.BeginInvoke((MethodInvoker)delegate
+                {
+                    buttonStart.Text = "Stop";
+                });
+                numericUpDownCurrentTime.BeginInvoke((MethodInvoker)delegate
+                {
+                    numericUpDownCurrentTime.Value = currentTime;
+                });
+                System.Threading.Thread.Sleep(delayTime);
 
-            } while (stopThread);
-            return;
+                if (stopThread)
+                {
+                    return;
+                }
+            }
 
-            image1 = new Bitmap(meshSizeX, meshSizeY);
-            drawUniverse(image1);
-            pictureBox1.Image = image1;
-            //buttonStart.Text = "Pulse";
-            System.Threading.Thread.Sleep(delayTime);
-            //buttonStart.Text = "Koniec";
-            maxTime++;
-            currentTime++;
-            
-            //if (stopThread)
-            //{
-            //    return;
-            //}
         }
 
-        private void StartThread()
+        private void StartThreadEvolution()
         {
             if (workThread == null)
             {
@@ -96,7 +304,7 @@ namespace KZ_CA_1D
             }
         }
 
-        private void StopThread()
+        private void StopThreadEvolution()
         {
             if (workThread != null)
             {
@@ -105,33 +313,60 @@ namespace KZ_CA_1D
                 workThread.Join(); // This makes the code here pause until the Thread exits.
 
                 workThread = null;
+                
             }
         }
 
-        void drawUniverse(Bitmap universeImage)
+        void drawUniverse()//Zmienić żeby rysowało tylko dany czas
         {
             for (int x = 1; x < meshSizeX + 1; x++)
             {
                 for (int y = 1; y < meshSizeY + 1; y++)
                 {
-                    if (universe[x, y] == 1) universeImage.SetPixel(x - 1, y - 1, Color.Black);
-                    else universeImage.SetPixel(x - 1, y - 1, Color.Yellow);
+                    
+                    if (currentUniverse[x,y] == 1)
+                    {
+                        dataGridView1.Rows[x-1].Cells[y-1].Style.BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[x-1].Cells[y-1].Style.BackColor = Color.Yellow;
+                       
+                    }
                 }
             }
-            image1 = universeImage;
         }
 
 
         private void buttonStart_Click(object sender, EventArgs e) // Trzeba to na nowych wątkach
         {
+            if (reset == true)
+            {
+                initEvolution2D();
+            }
             flag = !flag;
             if (flag == true)
             {
-                initEvolution2D();
-                startEvolution2D();
-                StartThread();
+                numericUpDownCurrentTime.Maximum = int.MaxValue;
+                trackBarCurrentTime.Maximum = int.MaxValue;
+                trackBarCurrentTime.Enabled = false;
+                numericUpDownCurrentTime.Enabled = false;
+                reset = false;
+                setDataGridView();
+                StartThreadEvolution();
+
             }
-            else StopThread();
+            else
+            {
+                StopThreadEvolution();
+                buttonStart.Text = "Start";
+                trackBarCurrentTime.Enabled = true;
+                numericUpDownCurrentTime.Enabled = true;
+                numericUpDownCurrentTime.Maximum = maxTime;
+                numericUpDownCurrentTime.Value = currentTime;
+                trackBarCurrentTime.Maximum = maxTime;
+                reset = true;
+            }
 
         }
 
@@ -143,7 +378,75 @@ namespace KZ_CA_1D
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
-            currentTime = trackBarCurrentTime.Value;
+            if (workThread == null)
+            {
+                currentTime = trackBarCurrentTime.Value;
+                numericUpDownCurrentTime.Value = currentTime;
+                currentUniverse = timespace[currentTime];
+                drawUniverse();
+            }
+        }
+
+        private void numericUpDownCurrentTime_ValueChanged(object sender, EventArgs e)
+        {
+            if (workThread == null)
+            {
+                currentTime = (int)numericUpDownCurrentTime.Value;
+                trackBarCurrentTime.Value = currentTime;
+                currentUniverse = timespace[currentTime];
+                drawUniverse();
+            }
+        }
+
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (true)
+            {
+                int x = e.RowIndex;
+                int y = e.ColumnIndex;
+                currentUniverse[x + 1, y + 1] = 1;
+                timespace[currentTime] = (int[,])currentUniverse.Clone();
+                drawUniverse();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)//Zmiana tego resetuje
+        {
+            if (workThread == null)
+            {
+                numericUpDownCurrentTime.Maximum = int.MaxValue;
+                trackBarCurrentTime.Maximum = int.MaxValue;
+                trackBarCurrentTime.Enabled = false;
+                numericUpDownCurrentTime.Enabled = false;
+                setDataGridView();
+                initEvolution2D();
+                seed();
+                reset = false;
+                drawUniverse();
+            }
+        }
+
+        private void buttonFaster_Click(object sender, EventArgs e)
+        {
+            delayTime = delayTime / 2;
+        }
+
+        private void buttonSlower_Click(object sender, EventArgs e)
+        {
+            delayTime = delayTime * 2;
+        }
+
+        private void numericUpDownMeshSizeX_ValueChanged(object sender, EventArgs e)
+        {
+            meshSizeX = (uint)numericUpDownMeshSizeX.Value;
+            reset = true;
+        }
+
+        private void numericUpDownMeshSizeY_ValueChanged(object sender, EventArgs e)
+        {
+            meshSizeY = (uint)numericUpDownMeshSizeY.Value;
+            reset = true;
         }
     }
 }
